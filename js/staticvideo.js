05fc470ecd545b978ea0799c336122b9b7fcabe2
Chetan Singh Rajawat <=chetan.rajawat25@gmail.com>
2024-07-02 13:48:47 +0530

    footer effect & static video animation

---
+++ js/staticvideo.js
@@ -0,0 +1,19 @@
+let projects = document.querySelectorAll("#page4 .project");
+let videoWrapper = document.querySelector("#static-video");
+let video = document.querySelector("#static-video video");
+projects.forEach((project)=>{
+    project.addEventListener("mouseenter",()=>{
+        videoWrapper.style.opacity = 1; 
+       
+        let videoLink = project.getAttribute("videoLink");
+        video.setAttribute("src", videoLink);
+    },true);
+
+    project.addEventListener("mouseleave",()=>{
+        videoWrapper.style.opacity = 0; 
+       
+     });
+ },true)
+
+console.log(projects);
+
